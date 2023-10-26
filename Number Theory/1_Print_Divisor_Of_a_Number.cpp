1c7af0a7affbbfd1550278ff527702444ac9045f
emon49 <abdula.al.emon@tigerit.com>
2023-10-26 16:04:24 +0600

    Count Number of Divisor(NOD)

---
+++ Number Theory/1_Print_Divisor_Of_a_Number.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> divisor;
+    for(int i=1; i*i<=n; i++){
+        if( (n%i) == 0){
+            divisor.push_back(i);
+            if( (n/i) != i)    divisor.push_back(n/i);
+        } 
+    }
+    sort(divisor.begin(), divisor.end());
+    for(auto u:divisor) cout<< u<<" ";
+}
