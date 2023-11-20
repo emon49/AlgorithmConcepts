d7ecbfbb49c6efdf124767a03fa5883b9f7e212b
emon49 <rafiemon71@gmail.com>
2023-11-21 05:15:15 +0600

    Cycle detection using DFS & BFS in undirected graph

---
+++ Graph/4_Cycle_Detection_Undirected_Graph_BFS.cpp
@@ -0,0 +1,45 @@
+#include <queue>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isCycleBFS(vector<vector<int>> &adjList, vector<bool> &visited, vector<int> &parent, int src){
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    parent[src] = -1;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        for(auto neighbour:adjList[front]){
+            if(visited[neighbour] == true && neighbour != parent[front])    return true;
+            if (!visited[neighbour]) {
+              q.push(neighbour);
+              visited[neighbour] = true;
+              parent[neighbour] = front;
+            }
+        }
+    }
+    return false;
+}
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    vector<vector<int>> adjList(n+1);
+    int u,v;
+    vector<bool> visited(n+1, false);
+    vector<int> parent(n+1);
+    for(int i=0; i<edges.size(); i++){
+        u= edges[i][0];
+        v= edges[i][1];
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            if(isCycleBFS(adjList, visited, parent, i)){
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+}
---
+++ Graph/5_Cycle_Detection_Undirected_Graph_DFS.cpp
@@ -0,0 +1,76 @@
+#include <queue>
+#include <vector>
+#include <string>
+using namespace std;
+bool isCycleDFS(vector<vector<int>> &adjList, vector<bool> &visited, int parent, int src){
+    visited[src] = true;
+    for(auto neighbour:adjList[src]){
+        if(!visited[neighbour]){
+            bool res =  isCycleDFS(adjList,visited,src,neighbour);
+            if(res) return true;
+        }
+         else if(parent != neighbour)  return true;
+    }
+    return false;
+}
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    vector<vector<int>> adjList(n+1);
+    int u,v;
+    vector<bool> visited(n+1, false);
+    for(int i=0; i<edges.size(); i++){
+        u= edges[i][0];
+        v= edges[i][1];
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            if(isCycleDFS(adjList, visited, -1, i)){
+                return "Yes";
+            }
+        }
+    }
+    return "No";
+}
+
+/*
+bool isCycleDFS(unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited,
+int node, int parent){
+    visited[node] = true;
+    
+    for(auto neighbour:adjList[node]){
+        if(!visited[neighbour]){
+            bool cycleDetected = isCycleDFS(adjList, visited, neighbour, node);
+            if(cycleDetected)
+                return true;
+        }
+        else if(neighbour!=parent){
+            return true;
+        }
+    }
+    return false;
+}
+string cycleDetection (vector<vector<int>>& edges, int n, int m)
+{
+    unordered_map<int, list<int>> adjList;
+    unordered_map<int, bool> visited;
+    //unordered_map<int,int> parent;
+    int u,v;
+    for(int i=0; i<m; i++){
+        u= edges[i][0];
+        v= edges[i][1];
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    for(int i=0; i<n; i++){
+        if(!visited[i]){
+            //bool ans = isCycle(adjList, visited, i, parent);
+            bool ans = isCycleDFS(adjList, visited, i, -1);
+            if(ans)
+                return "Yes";
+        }
+    }
+    return "No";   
+}
+*/
